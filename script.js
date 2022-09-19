a47f4f616d4b5c0b0477174020fcb389741e318d
Dave <70270638+lostSky-a11y@users.noreply.github.com>
2022-09-19 10:21:38 +0300

    Mobile Design

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
   $(".sub_menu").hide();
+
+  const menu_icon = [
+    "./images/icon-menu.svg",
+    "./images/icon-close-menu.svg"
+  ]
+  var n = 1;
+  $("#hamburger_menu").click(function(){
+    n++;
+    if(n > 1){
+      n = 0;
+    }
+    $("#hamburger_menu").attr("src", menu_icon[n]);
+    $("nav").children("ul").slideToggle();
+    if($("#hamburger_menu").attr("src") == "./images/icon-close-menu.svg"){
+      $("body").css({
+      "background-color":"hsl(0, 0%, 41%)"
+    });
+    } else {
+      $("body").css({
+      "background-color":"hsl(0, 0%, 98%)"
+    });
+    $(".menu").show();
+    }
+  });
+  
 });
 function mouse_hover(element_id){
 $("#"+element_id).children("img").attr("src", "./images/icon-arrow-up.svg");
@@ -11,5 +36,11 @@ $("#"+element_id).children("img").attr("src", "./images/icon-arrow-down.svg");
 $("."+element_id+"_sub_menu").hide();
 //$("."+element_id+"_sub_menu").css("display", "none");
 }
+
+
+
+
+
+
 
   
